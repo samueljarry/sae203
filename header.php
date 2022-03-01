55611329f5311127844748719b26e708466581ee
samueljarry <99248569+samueljarry@users.noreply.github.com>
2022-03-01 22:42:37 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script>
+        function show() {
+            document.getElementById('sidebar').classList.toggle('active')
+            }
+    </script>
+    <! –– POLICES ––>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/lve5zse.css">
+
+</head>
+<header>
+    <div id="sidebar" onclick="show()">
+
+        <div class="btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="listing.php">Nos produits</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+    <div id="navbar">
+            <nav>
+            <div class="logo">
+                <a href="index.php"><img src="images/eclothing.png"></a>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ listing.php
@@ -0,0 +1,48 @@
+<?php
+    require('header.php');
+?>
+
+<body>
+<main>
+<div class="table-wrapper">
+    <table class="fl-table">
+        <thead>
+        <tr>
+            <th>Photo</th>
+            <th>Nom</th>
+            <th>Prix</th>
+            <th>Catégorie</th>
+            <th>Couleur</th>
+            <th>Tailles</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><img src="images/id1.png"></td>
+            <td>Veiled Puffer</td>
+            <td>170€</td>
+            <td>Veste</td>
+            <td><img src="couleurs/noir.jpg"></td>
+            <td>XS S M L</td>
+            
+        </tr>
+        <tr>
+            <td><img src="images/id2.jpg"></td>
+            <td>Veiled Puffer</td>
+            <td>170€</td>
+            <td>Veste</td>
+            <td><img src="couleurs/beige.jpg"></td>
+            <td>XS S M L</td>
+        </tr>
+        <tr>
+        <td><img src="images/id3.jpg"></td>
+            <td>Soldier Jacket</td>
+            <td>170€</td>
+            <td>Veste</td>
+            <td><img src="couleurs/beige.jpg"></td>
+            <td>XS S M L</td>
+        </tr>
+        </tbody>
+    </table>
+</main>
+</body>
