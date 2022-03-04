8f54a6cfa0ce4991104119a784387ff1d6b0503f
samueljarry <samueljarry.sj@gmail.com>
2022-03-04 15:32:10 +0100

    reponse_recherche.php

--- reponse_recherche.php
+++ reponse_recherche.php
@@ -1,3 +1,14 @@
 <?php
-    require('header.php');
-?>
+    if ( (empty($_POST['prenom'])) || (empty($_POST['age'])) ) {
+        header('Location: form2.php');
+    }
+
+    $newstr = filter_var($_POST['prenom'],
+    FILTER_SANITIZE_STRING);
+     
+    echo $newstr;
+    $newstr = filter_var($_POST['age'],
+    FILTER_SANITIZE_STRING);
+     
+    echo $newstr;
+    ?>
